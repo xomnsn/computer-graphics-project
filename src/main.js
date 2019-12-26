70d5300fe6a95ef8f1bbfcdcd3b066fab3d1bc6e
xomnsn <xomnsn@gmail.com>
2019-12-26 14:05:22 +0300

    made grid control panel/canvas added play/pause button fixed dot position working on caching doesn't work: cache deletes first dot on every round

--- src/main.js
+++ src/main.js
@@ -58,14 +58,19 @@ function init() {
     var baseR = 250;
     var rotatorR = 74;
     var pen = 50;
-    var angle = 10;
+    var angle = 15;
     var angleBase = rotatorR * angle / baseR;
+
+    //----------------------------EVENT LISTENERS---------------------------
+    $( "#pause" ).on( "click", togglePause);
 
 
     //---------------------CREATE CANVAS AND STAGE--------------------------
     var canvas = document.getElementById("game");
-    var c = canvas.getContext("2d");
-    var stage = new createjs.Stage(canvas)
+    var canvasContainer = document.getElementById("canvas-container");
+    canvas.width = canvasContainer.offsetWidth;
+    canvas.height = canvasContainer.offsetHeight;
+    var stage = new createjs.Stage(canvas);
     stage.x = canvas.width / 2;
     stage.y = canvas.height / 2;
 
@@ -76,30 +81,104 @@ function init() {
     //---------------------------INITIAL DRAW--------------------------------
     drawXY(stage);
     var dotContainer = new createjs.Container;
-    dotContainer.cache(-baseR, -baseR, 2 * baseR, 2 * baseR)
+    var cacheContainer = new createjs.Container;
+    cacheContainer.cache(-baseR, -baseR, 2 * baseR, 2 * baseR);
     var baseContainer = newBase(baseR);
     var rotatorContainer = newRotator(rotatorR, baseR, pen);
     baseContainer.addChild(rotatorContainer);
     stage.addChild(dotContainer);
+    stage.addChild(cacheContainer);
     stage.update();
+
 
     //------------------------------TICKER----------------------------------
     createjs.Ticker.framerate = 60;
     createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
     createjs.Ticker.on("tick", tick);
+    createjs.Ticker.setPaused(true);
 
-    var i = 0;
+    var i = 1;
     function tick() {
         //animation cycle
-        i += 1;
-        if (i > 10) {
-            i = 0;
-            dotContainer.updateCache();
+        if (!createjs.Ticker.getPaused()) {
+            if (i > 100) {
+                console.log(dotContainer.getNumChildren());
+                i = 1;
+                var numOfCycles = 0;
+                for(var j = 0; j < 100; j++) {
+                    numOfCycles += 1;
+                    var thisChild = dotContainer.getChildAt(i);
+                    cacheContainer.addChild(thisChild);
+                }
+                console.log("num of cycles " + numOfCycles);
+                dotContainer.removeAllChildren();
+                cacheContainer.updateCache();
+            }
+            baseContainer.rotation -= angleBase;
+            rotatorContainer.rotation += angle + angleBase;
+            var dot = newDot(pen, rotatorContainer, dotContainer);
+            dotContainer.addChild(dot);
+            i += 1;
         }
-        baseContainer.rotation -= angleBase;
-        rotatorContainer.rotation += angle + angleBase;
-        var dot = newDot(pen, rotatorContainer, dotContainer);
-        dotContainer.addChild(dot);
+
         stage.update();
     }
+
+    function togglePause() {
+        var paused = !createjs.Ticker.getPaused();
+        createjs.Ticker.setPaused(paused);
+        $(this).text(paused ? "Play" : "Pause");
+    }
+
 }
+//
+//
+//
+//
+// var stage, pauseCircle, goCircle, output;
+//
+// function init() {
+//     stage = new createjs.Stage("demoCanvas");
+//
+//     pauseCircle = new createjs.Shape();
+//     pauseCircle.graphics.beginFill("red").drawCircle(0, 0, 40);
+//     pauseCircle.y = 50;
+//     stage.addChild(pauseCircle);
+//
+//     goCircle = new createjs.Shape();
+//     goCircle.graphics.beginFill("green").drawCircle(0, 0, 40);
+//     goCircle.y = 150;
+//     stage.addChild(goCircle);
+//
+//     // and register our main listener
+//     createjs.Ticker.addEventListener("tick", tick);
+//
+//     // UI code:
+//     output = stage.addChild(new createjs.Text("", "14px monospace", "#000"));
+//     output.lineHeight = 15;
+//     output.textBaseline = "top";
+//     output.x = 10;
+//     output.y = stage.canvas.height-output.lineHeight*3-10;
+// }
+//
+// function tick(event) {
+//     goCircle.x += 10;
+//     if (goCircle.x > stage.canvas.width) { goCircle.x = 0; }
+//
+//     if (!createjs.Ticker.getPaused()) {
+//         pauseCircle.x += 10;
+//         if (pauseCircle.x > stage.canvas.width) { pauseCircle.x = 0; }
+//     }
+//
+//     output.text = "getPaused()    = "+createjs.Ticker.getPaused()+"\n"+
+//         "getTime(true)  = "+createjs.Ticker.getTime(true)+"\n"+
+//         "getTime(false) = "+createjs.Ticker.getTime(false);
+//
+//     stage.update(event); // important!!
+// }
+//
+// function togglePause() {
+//     var paused = !createjs.Ticker.getPaused();
+//     createjs.Ticker.setPaused(paused);
+//     document.getElementById("pauseBtn").value = paused ? "unpause" : "pause";
+// }
